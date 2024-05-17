dbaa7ca5662a720e3fc0d46b74773086688f13c7
DevGentil <devigentil@gmail.com>
2024-05-17 19:45:09 -0300

    adicionando Sections

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,21 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <section title = "Links úteis">
+        <Links>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+          <li><a href="#">https://rocketseat.com.br</a></li>
+        </Links>
+      </section>
+
       <Button title="Voltar" />
     </Container>
   )
