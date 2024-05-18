44c0f511fb0b8d8b69b51e59cb7093a1f6e5ce9c
DevGentil <devigentil@gmail.com>
2024-05-18 17:09:47 -0300

    Finalizando details

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -6,5 +6,5 @@ export function Section({ title, children }) {
         <h2>{title}</h2>
         {children}
     </Container>
-    );
+    )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,27 +1,47 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
+import { Tag } from '../../components/Tag'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
-import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
+
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <section title = "Links úteis">
-        <Links>
-          <li><a href="#">https://rocketseat.com.br</a></li>
-          <li><a href="#">https://rocketseat.com.br</a></li>
-        </Links>
-      </section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <section title='Marcadores'>
-        <Tag title="Express" />
-        <Tag title="Node" />
-      </section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Voltar" />
+          <p>
+            Em apenas uma semana você vai dominar na praça o que é React.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
