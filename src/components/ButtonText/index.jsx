0e642c6c67ca2d9fea80228c12889305a15e4681
DevGentil <devigentil@gmail.com>
2024-05-18 17:56:14 -0300

    corrigindo isactive

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 
 export function ButtonText({ title, isActive = false, ...rest }) {
     return (
-        <Container type="button" isActive {...rest}>
+        <Container type="button" $isActive={isActive.toString()} {...rest}>
             {title}
         </Container>
     )
