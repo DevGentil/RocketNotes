5543dfa205c2251c2a8f75125c28550916157e0c
DevGentil <devigentil@gmail.com>
2024-05-18 18:20:49 -0300

    adicionando componente Input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,39 +1,37 @@
+import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
-    return (
-        <Container>
-            <Brand>
-                <h1>Rocketnotes</h1>
-            </Brand>
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
 
-            <Header />
+      <Header />
 
-            <Menu>
+      <Menu>
+        <li><ButtonText title="Todos" $isactive /></li>
+        <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Nodejs" /></li>
+      </Menu>
 
-                <li><ButtonText title="Todos" /></li>
-                <li><ButtonText title="React" /></li>
-                <li><ButtonText title="Node" /></li>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
 
-            </Menu>
+      <Content>
 
-            <Search>
+      </Content>
 
-
-            </Search>
-
-            <Content>
-
-
-            </Content>
-
-            <NewNote>
-
-
-            </NewNote>
-        </Container>
-    )
-}    
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  )
+}
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,60 +1,67 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const Container  = styled.div`
-    width: 100%;
-    height: 100vh;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 
-    display: grid;
-    grid-template-columns: 250px auto;
-    grid-template-rows: 105px 128px auto 64px;
-    grid-template-areas:
-    "brand header"
-    "menu header"
-    "menu content"
-    "newnote content";
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px auto 64px;
+  grid-template-areas: 
+  "brand header"
+  "menu search"
+  "menu content"
+  "newnote content";
 
-    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
-`;
-
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+`
 export const Brand = styled.div`
-    grid-area: brand;
+  grid-area: brand;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    border-bottom-width: 1px;
-    border-bottom-style: solid;
-    border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
-    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
-    > h1 {
-        font-size: 24px;
-        color: ${({ theme }) => theme.COLORS.ORANGE};
-    }
-`;
-
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
+`
 export const Menu = styled.ul`
-    grid-area: menu;
-    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  grid-area: menu;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
-    padding-top: 64px;
-    text-align: center;
+  padding-top: 64px;
+  text-align: center;
 
-    > li {
-        margin-bottom: 24px;
-    }
-`;
+  > li {
+    margin-bottom: 24px;
+  }
+`
+export const Search = styled.div`
+  grid-area: search;
+  padding: 64px 64px 0;
+`
+export const Content = styled.div`
+  grid-area: content;
+`
+export const NewNote = styled.button`
+  grid-area: newnote;
 
-export const Search   = styled.div`
-    grid-area: search;
-`;
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+  border: none;
 
-export const Content   = styled.div`
-    grid-area: content;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-export const NewNote  = styled.button`
-    grid-area: newnote;
-`;
+  svg {
+    margin-right: 8px;
+  }
+`
